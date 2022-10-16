e93fd7f42fefbb45277be2919e3db0c78aecae0f
Vihanga Weerasinghe <vihanga1432@gmail.com>
2022-10-16 16:28:54 +0100

    initial file upload

---
+++ conn.js
@@ -0,0 +1,6 @@
+// This file handles connections to the backend Swift AIO server
+// Do not make changes in here as it could prevent your module from registering!
+
+console.log("Connecting to backend");
+
+// Turtlee was here...
---
+++ index.js
@@ -0,0 +1,3 @@
+const server = require("./conn.js");
+
+console.log("Starting module")
