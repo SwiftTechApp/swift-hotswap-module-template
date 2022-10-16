22e4e41d2cb9653627508cc473b8e5b1281f1e1b
Vihanga Weerasinghe <vihanga1432@gmail.com>
2022-10-16 17:11:45 +0100

    added build command

--- conn.js
+++ conn.js
@@ -1,6 +1,8 @@
 // This file handles connections to the backend Swift AIO server
 // Do not make changes in here as it could prevent your module from registering!
 
+const ws = require("websockets").client;
+
 console.log("Connecting to backend");
 
 // Turtlee was here...
